5bff72750e313e6d77f9d52fae13238af00eb3d7
Jacques Giraudel <me@jacquesgiraudel.com>
2018-08-25 15:40:12 +0200

    Step 4 - removing the side effect by using the functional operator 'plus' to return a new list (a new state of the list)

--- app/src/main/java/fr/jgl/intropgmfonct/FindByTitleKotlin.kt
+++ app/src/main/java/fr/jgl/intropgmfonct/FindByTitleKotlin.kt
@@ -6,31 +6,28 @@ class FindByTitleKotlin {
 
         // findByTitle :: (String, [Movie]) -> [Movie]
         fun findByTitle(query: String, collection: MutableList<Movie>): List<Movie>{
-            var results: MutableList<Movie> = mutableListOf()
-
-            // Use of functions as variables (predicate and add) : functions as first class citizen
+            var results: List<Movie> = listOf()
 
             // matches :: (String, Film) -> Boolean
             val predicate = ::matches
 
-            // TODO side effect moved up (still on results)
             // add :: (Film) -> Boolean
-            val add = fun (movie: Movie) = results.add(movie)
+            val add = fun (movie: Movie, movies: List<Movie>) = movies.plus(movie)
 
             for (movie: Movie in collection){
                 val fn = addIf(predicate, query, movie, add)
-                fn(movie)
+                results = fn(movie, results)
             }
 
             return results
         }
 
         // addIfMatches :: ((String, Movie) -> Boolean, String, Movie, [Movie] -> (Boolean)) -> (Movie) -> (Boolean)
-        fun addIf(predicate: (String, Movie) -> Boolean, query: String, movie: Movie, add: (Movie) -> (Boolean)): (Movie) -> (Boolean){
+        fun addIf(predicate: (String, Movie) -> Boolean, query: String, movie: Movie, add: (Movie, List<Movie>) -> (List<Movie>)): (Movie, List<Movie>) -> (List<Movie>){
             if (predicate(query, movie)){
                 return add
             }
-            return fun (movie: Movie) = false
+            return fun (movie: Movie, movies: List<Movie>) = listOf<Movie>()
         }
 
         // matches :: (String, Film) -> Boolean
