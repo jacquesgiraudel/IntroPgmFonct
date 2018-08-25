4795bb52065f34a4c4d7530d956527d8f948ade7
Jacques Giraudel <me@jacquesgiraudel.com>
2018-08-25 14:14:25 +0200

    Step 1 - refactoring to procedural programming

--- app/src/main/java/fr/jgl/intropgmfonct/FindByTitleKotlin.kt
+++ app/src/main/java/fr/jgl/intropgmfonct/FindByTitleKotlin.kt
@@ -3,19 +3,30 @@ package fr.jgl.pgmfonct
 class FindByTitleKotlin {
 
     companion object {
+
         fun findByTitle(query: String, collection: MutableList<Movie>): List<Movie>{
             var results: MutableList<Movie> = mutableListOf()
 
             do {
                 val movie = collection.removeAt(0)
-                if (movie.title.contains(query)){
-                    results.add(movie)
-                }
+                addIfMatches(query, movie, results)
             }
             while (collection.size > 0)
 
             return results
         }
+
+        fun addIfMatches(query: String, movie: Movie, results: MutableList<Movie>){
+            if (matches(query, movie)){
+                results.add(movie)
+            }
+        }
+
+        fun matches(query: String, movie: Movie): Boolean = title(movie).isInfixOf(query)
+
+        fun title(movie: Movie): String = movie.title
+
+        fun String.isInfixOf(query: String) = contains(query)
 
     }
 }
