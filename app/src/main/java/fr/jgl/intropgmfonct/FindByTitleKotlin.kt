27fa077090dd372e2820511c230d630d63df0b7b
Jacques Giraudel <me@jacquesgiraudel.com>
2018-08-25 14:41:26 +0200

    Step 3 - moving up the side effect with the help of functions as variables (functions as first class citizen)

--- app/src/main/java/fr/jgl/intropgmfonct/FindByTitleKotlin.kt
+++ app/src/main/java/fr/jgl/intropgmfonct/FindByTitleKotlin.kt
@@ -4,29 +4,42 @@ class FindByTitleKotlin {
 
     companion object {
 
+        // findByTitle :: (String, [Movie]) -> [Movie]
         fun findByTitle(query: String, collection: MutableList<Movie>): List<Movie>{
             var results: MutableList<Movie> = mutableListOf()
 
-            do {
-                val movie = collection.removeAt(0)
-                addIfMatches(query, movie, results)
+            // Use of functions as variables (predicate and add) : functions as first class citizen
+
+            // matches :: (String, Film) -> Boolean
+            val predicate = ::matches
+
+            // TODO side effect moved up (still on results)
+            // add :: (Film) -> Boolean
+            val add = fun (movie: Movie) = results.add(movie)
+
+            for (movie: Movie in collection){
+                val fn = addIf(predicate, query, movie, add)
+                fn(movie)
             }
-            while (collection.size > 0)
 
             return results
         }
 
-        // TODO Side effect (results, because modified outside its local environment)
-        fun addIfMatches(query: String, movie: Movie, results: MutableList<Movie>){
-            if (matches(query, movie)){
-                results.add(movie)
+        // addIfMatches :: ((String, Movie) -> Boolean, String, Movie, [Movie] -> (Boolean)) -> (Movie) -> (Boolean)
+        fun addIf(predicate: (String, Movie) -> Boolean, query: String, movie: Movie, add: (Movie) -> (Boolean)): (Movie) -> (Boolean){
+            if (predicate(query, movie)){
+                return add
             }
+            return fun (movie: Movie) = false
         }
 
+        // matches :: (String, Film) -> Boolean
         fun matches(query: String, movie: Movie): Boolean = title(movie).isInfixOf(query)
 
+        // title :: (Film) -> String
         fun title(movie: Movie): String = movie.title
 
+        // isInfixOf :: (String, String) -> Boolean
         fun String.isInfixOf(query: String) = contains(query)
 
     }
